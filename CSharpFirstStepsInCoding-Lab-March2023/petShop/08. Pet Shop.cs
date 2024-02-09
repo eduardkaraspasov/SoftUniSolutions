76db85cf2f25dcfc30bb7c4a6e61e08b09255db8
eduardkaraspasov <106177199+eduardkaraspasov@users.noreply.github.com>
2024-02-09 23:59:33 +0200

    Add files via upload

---
+++ CSharpFirstStepsInCoding-Lab-March2023/petShop/08. Pet Shop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace petShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dogFood = double.Parse(Console.ReadLine());
+            double catFood = double.Parse(Console.ReadLine());
+            double dogFoodPrice = dogFood * 2.50;
+            double catFoodPrice = catFood * 4;
+
+            double finalPrice = dogFoodPrice + catFoodPrice;
+
+            Console.WriteLine($"{finalPrice} lv.");
+            
+
+
+        }
+    }
+}
