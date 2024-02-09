ecbf365690f3210e9300ea406c60afce069b4d17
eduardkaraspasov <106177199+eduardkaraspasov@users.noreply.github.com>
2024-02-09 23:52:29 +0200

    Add files via upload

---
+++ CSharpFirstStepsInCoding-Lab-March2023/inchesToCentimeters/04. Inches to Centimeters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace rectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch = double.Parse(Console.ReadLine());
+            double centemeters = inch * 2.54;
+            Console.WriteLine(centemeters);
+        }
+    }
+}
